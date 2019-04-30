5750f8a2b5c5f76135474b509742188b8cf7f27b
zulfiqartariq <zulfiqar.tariq@coeus-solutions.de>
2019-04-30 11:29:05 +0500

    first pacakge

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Acme\TestBundle\Controller;
+namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -9,5 +9,9 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('AcmeTestBundle:Default:index.html.twig');
+    }
+
+    public static function test(){
+        echo "Hi this";
     }
 }
